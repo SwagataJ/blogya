39efe954c87a00f21a02d58e0f3e0b71f62580c2
Swagata Jana <65404204+SwagataJ@users.noreply.github.com>
2021-05-09 19:04:21 +0530

    Fixed typos

--- email_isauthenticated.py
+++ email_isauthenticated.py
@@ -7,7 +7,7 @@ import os
 
 def validate_email(email):
     api_key = os.environ.get('REALEMAIL_API_KEY')
-    url = 'https://realemail.expeditedaddons.com/?api_key=' + \
+    url = 'https://realemail.expeditedaddons.com/?api_key=' + 
         api_key + '&email=' + email + '&fix_typos=false'
     resp = urllib.request.urlopen(url, context=ssl.create_default_context(
         cafile=certifi.where())).read().decode('utf-8')
