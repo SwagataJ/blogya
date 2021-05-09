cc0ce5b5f2847180f7f6c4af074aae183bb31e06
Swagata Jana <65404204+SwagataJ@users.noreply.github.com>
2021-05-09 18:53:07 +0530

    Add an email verifier

---
+++ email_isauthenticated.py
@@ -0,0 +1,17 @@
+import json
+import ssl
+import urllib.request
+import certifi
+import os
+
+
+def validate_email(email):
+    api_key = os.environ.get('REALEMAIL_API_KEY')
+    url = 'https://realemail.expeditedaddons.com/?api_key=' + api_key + '&email=' + email + '&fix_typos=false'
+    resp = urllib.request.urlopen(url, context=ssl.create_default_context(cafile=certifi.where())).read().decode('utf-8')
+    json_acceptable_string = resp.replace("'", "\"")
+    response = json.loads(json_acceptable_string)
+    return response['valid'] and not response['is_disposable']
+
+
+
